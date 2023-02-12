406d69712073bbc04ba0ffb4ad0ba62ca314730f
Lucas Nguyen <lucasoAlt@outlook.com>
2023-02-12 19:51:22 +0100

    Update api.js

--- astro-frontend/src/lib/api.js
+++ astro-frontend/src/lib/api.js
@@ -6,3 +6,10 @@ export async function getAllPosts() {
     const posts = await client.fetch(query)
     return posts
 }
+
+export async function getAllAuthors() {
+    const client = useSanityClient()
+    const query = `*[_type == "author"]`;
+    const authors = await client.fetch(query)
+    return authors
+}
